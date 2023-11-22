80e9a703cede3693719c9bdf42ba2d0beeeca0c3
Radimilahy Rado Nomena <radimilahy@uni-potsdam.de>
2023-11-22 17:55:42 +0100

    Uploading the rep

--- Topic_modeling/BERTopic.py
+++ Topic_modeling/BERTopic.py
@@ -1,5 +1,6 @@
 from bertopic import BERTopic
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("C:/Users/nomen/Twitter_Data_Analysis/Topic_modeling/discussion_cleaned.csv", encoding='utf-8')
 docs = data.Text.tolist()
@@ -16,20 +17,25 @@ topic_model = BERTopic(embedding_model="all-MiniLM-L6-v2")
 topics, probs = topic_model.fit_transform(docs)
 
 # displays the topic number. -1 are the outliers
-topic_model.get_topic_info()
+topic_info = topic_model.get_topic_info()
+print(topic_info)
 
-# gets more words
-topic_model.get_topic(0)
+# Get more words for topic 0
+topic_0_words = topic_model.get_topic(0)
+print(topic_0_words)
 
-# like findThoughts
-topic_model.get_representative_docs(0)
+# Get representative documents for topic 0
+representative_docs_topic_0 = topic_model.get_representative_docs(0)
+print(representative_docs_topic_0)
 
-# see the dat with cluster assigned to it
+# see the dat with cluster assigned to it. Create a DataFrame with the assigned topics for each document
 df = pd.DataFrame({"topic": topics, "document": docs})
 print(df)
 
 # visualize, the larger the circle the bigger the cluster
 # the closer they are the more semantically similar they are
-print(topic_model.visualize_topics())
+topic_model.visualize_topics()
+plt.show()
 
-print(topic_model.visualize_barchart())
+topic_model.visualize_barchart()
+plt.show()
